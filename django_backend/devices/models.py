2cfee3884e136cb8423fd98c3e557cfe9562860a
evanmantan <evanmanuel1674@gmail.com>
2024-10-16 01:12:04 +0700

    Backend: Update model to accomodate probability and diagnosis for BCa

--- django_backend/devices/models.py
+++ django_backend/devices/models.py
@@ -18,20 +18,37 @@ class Reading(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
     # TODO: Add other info fields according to hardware
-    data1 = models.FloatField(default=0, null=True)
-    data2 = models.FloatField(default=0, null=True)
-    data3 = models.FloatField(default=0, null=True)
-    data4 = models.FloatField(default=0, null=True)
-    data5 = models.FloatField(default=0, null=True)
-    data6 = models.FloatField(default=0, null=True)
-    data7 = models.FloatField(default=0, null=True)
-    data8 = models.FloatField(default=0, null=True)
-    data9 = models.FloatField(default=0, null=True)
-    data10 = models.FloatField(default=0, null=True)
-    data11 = models.FloatField(default=0, null=True)
+    data1 = models.FloatField(default=0)
+    data2 = models.FloatField(default=0)
+    data3 = models.FloatField(default=0)
+    data4 = models.FloatField(default=0)
+    data5 = models.FloatField(default=0)
+    data6 = models.FloatField(default=0)
+    data7 = models.FloatField(default=0)
+    data8 = models.FloatField(default=0)
+    data9 = models.FloatField(default=0)
+    data10 = models.FloatField(default=0)
+    data11 = models.FloatField(default=0)
+    probability = models.FloatField(default=0)
+    diagnosis = models.BooleanField(default=False)
 
     def __str__(self) -> str:
         return f"Reading {self.id} from {self.device}"
+
+    def save(self, *args, **kwargs):
+        if self.data2 <= 1506.64625:
+            self.probability += 20
+        if self.data3 <= 3642.04625:
+            self.probability += 20
+        if self.data4 <= 3214.433:
+            self.probability += 20
+        if self.data5 <= 2086.866:
+            self.probability += 20
+        if self.data6 <= 1309.0539:
+            self.probability += 20
+        self.diagnosis = self.probability >= 50
+
+        super().save(*args, **kwargs)
 
 
 class DeviceList(models.Model):
